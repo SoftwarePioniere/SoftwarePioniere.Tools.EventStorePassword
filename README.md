d9aa2076fb077e30067671090c040aa93fc364ae
Tobias Böker <tb@softwarepioniere.de>
2019-04-19 20:38:50 +0200

    added subcommands

--- README.md
+++ README.md
@@ -19,7 +19,26 @@ eventstore-password --help
 ## Development
 
 ```
+dotnet tool install -g GitVersion.Tool
+
 dotnet pack
 dotnet tool install --global --add-source ./nupkg SoftwarePioniere.Tools.EventStorePassword
 dotnet tool uninstall -g SoftwarePioniere.Tools.EventStorePassword
+
+
+```
+
+## Deployment
+
+```
+$j=(ConvertFrom-Json -InputObject  ([System.string]::Concat((dotnet-gitversion))))
+
+$vv=$j.NugetVersionV2
+$asv=$j.AssemblySemVer
+
+dotnet build -c Release -p:Version=$vv -p:AssemblyVersion=$asv -p:FileVersion=$asv
+dotnet pack -c Relase --no-build
+
+$pkg=(Get-ChildItem -Path .\nupkg\ -Filter "*$vv.nupkg").FullName
+dotnet nuget push $pkg -s https://api.nuget.org/v3/index.json -k $env:NUGET_API_KEY
 ```
--- SoftwarePioniere.Tools.EventStorePassword/Program.cs
+++ SoftwarePioniere.Tools.EventStorePassword/Program.cs
@@ -13,54 +13,138 @@ namespace SoftwarePioniere.Tools.EventStorePassword
     {
         static int Main(string[] args)
         {
-            var app = new CommandLineApplication();
-            app.HelpOption("-h|--help");
-
-            var optionUrl = app.Option("-u|--url <localhost>", "EventStore Http URL", CommandOptionType.SingleValue);
-            var optionPort = app.Option<int>("-p|--port <2113>", "Port", CommandOptionType.SingleValue);
-            var optionLogin = app.Option("-l|--login <admin>", "Login to Change Password", CommandOptionType.SingleValue);
-            var optionOldPasswort = app.Option("-o|--old <changeit>", "Old Password", CommandOptionType.SingleValue);
-            var optionNewPasswort = app.Option("-n|--new <changed>", "New Password", CommandOptionType.SingleValue);
-
-            app.OnExecute(async () =>
+            var app = new CommandLineApplication
             {
-                var url = optionUrl.HasValue() ? optionUrl.Value() : "localhost";
-                var port = optionPort.HasValue() ? optionPort.ParsedValue : 2113;
-                var login = optionLogin.HasValue() ? optionLogin.Value() : "admin";
-                var oldp = optionOldPasswort.HasValue() ? optionOldPasswort.Value() : "changeit";
-                var newp = optionNewPasswort.HasValue() ? optionNewPasswort.Value() : "changed";
+                Name = "eventstore-password",
+                Description = "Tool to set the Event Store Password",
+                LongVersionGetter = () => typeof(Program).Assembly.GetName().Version.ToString()
+            };
 
-                await ChangePasswordAsync(url, port, login, oldp, newp);
+            app.HelpOption("-h|--help", inherited: true);
+            app.Command("test",
+                cmd =>
+                {
+                    cmd.FullName = "Test Password";
+                    cmd.Description = "Test a Login with Password";
+
+                    var argLogin = cmd.Argument<string>("login", "Login Name");
+                    argLogin.IsRequired(errorMessage: "login empty");
+
+                    var argPassword = cmd.Argument<string>("password", "Password to Test");
+                    argPassword.IsRequired(errorMessage: "password empty");
+
+                    var optionUrl = cmd.Option("-u|--url", "EventStore Http URL [localhost]", CommandOptionType.SingleValue);
+                    var optionPort = cmd.Option<int>("-p|--port", "Port [2113]", CommandOptionType.SingleValue);
+
+                    cmd.OnExecute(async () =>
+                    {
+                        var url = optionUrl.HasValue() ? optionUrl.Value() : "localhost";
+                        var port = optionPort.HasValue() ? optionPort.ParsedValue : 2113;
+                        await TestPasswordAsync(url, port, argLogin.Value, argPassword.Value);
+                    });
+                });
+
+            app.Command("change", cmd =>
+            {
+                cmd.FullName = "Change the Password";
+                cmd.Description = "Change Login Password";
+
+                var argLogin = cmd.Argument<string>("login", "Login Name");
+                argLogin.IsRequired(errorMessage: "login empty");
+
+                var argOldPassword = cmd.Argument<string>("old_password", "Old Password");
+                argOldPassword.IsRequired(errorMessage: "old_password empty");
+
+                var argNewPassword = cmd.Argument<string>("new_password", "New Password");
+                argNewPassword.IsRequired(errorMessage: "new_password empty");
+
+                var optionUrl = cmd.Option("-u|--url", "EventStore Http URL [localhost]", CommandOptionType.SingleValue);
+                var optionPort = cmd.Option<int>("-p|--port", "Port [2113]", CommandOptionType.SingleValue);
+
+                cmd.OnExecute(async () =>
+                {
+                    var url = optionUrl.HasValue() ? optionUrl.Value() : "localhost";
+                    var port = optionPort.HasValue() ? optionPort.ParsedValue : 2113;
+
+                    var test = await TestPasswordAsync(url, port, argLogin.Value, argNewPassword.Value);
+
+                    if (!test)
+                    {
+                        await ChangePasswordAsync(url,
+                            port,
+                            argLogin.Value,
+                            argOldPassword.Value,
+                            argNewPassword.Value);
+                    }
+                });
+            });
+
+         
+            var optionVersion = app.Option("-v|--version", "Shows the Version", CommandOptionType.NoValue);
+
+
+            app.OnExecute(() =>
+            {
+                if (optionVersion.HasValue())
+                {
+                    app.ShowVersion();
+                    return 0;
+                }
+
+                app.ShowHelp();
+                return 1;
+
             });
 
             return app.Execute(args);
         }
 
-        private static async Task ChangePasswordAsync(string url, int port, string login, string oldPassword,
-            string newPassword)
+
+        private static async Task<bool> TestPasswordAsync(string url, int port, string login, string password)
+        {
+            var man = CreateUsersManager(url, port);
+
+            Console.WriteLine("Test Password for Login {0}", login);
+
+            try
+            {
+                Console.WriteLine("Trying Password");
+                await man.GetUserAsync(login, new UserCredentials(login, password));
+                Console.WriteLine("Password works");
+
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot access with Password");
+                var e1 = e.InnerException ?? e;
+                Console.WriteLine(e1.Message);
+            }
+
+            return false;
+        }
+
+        private static UsersManager CreateUsersManager(string url, int port)
         {
             var host = Dns.GetHostEntry(url);
             var ip = host.AddressList.First(x => x.ToString() != "::1");
 
-            Console.WriteLine("ChangePasswordAsync {0} {1} {2}", ip, port, login);
+            Console.WriteLine("URL IP: {0} {1}", url, ip);
 
             var man = new UsersManager(new ConsoleLogger(),
                 new IPEndPoint(ip, port),
                 TimeSpan.FromSeconds(1));
 
-            try
-            {
-                Console.WriteLine("Trying new Password");
-                await man.GetUserAsync(login, new UserCredentials(login, newPassword));
-                Console.WriteLine("New Password already set");
-                return;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Cannot access with new Password");
-                var e1 = e.InnerException ?? e;
-                Console.WriteLine(e1.Message);
-            }
+            return man;
+        }
+
+        private static async Task ChangePasswordAsync(string url, int port, string login, string oldPassword,
+            string newPassword)
+        {
+            var man = CreateUsersManager(url, port);
+
+            Console.WriteLine("Change Password for Login {0}", login);
 
             Console.WriteLine("Try change password");
             await man.ChangePasswordAsync(login, oldPassword, newPassword);
